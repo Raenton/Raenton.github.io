db140d0242ccd7ef98d04b4c6ce9466f0df814ee
Robin Denton <raenton@gmail.com>
2020-04-08 10:37:42 +0100

    Refactor menu listeners in index js

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 (function() {
   
-  function setupAnimationListeners() {
+  function setupMenu() {
     // menu animations
     const menuIcons = document.getElementById('menu-icons');
-    Array.from(menuIcons.children).forEach(node => {
-      node.addEventListener('mouseover', () => {
-        node.classList.add('mouseIn');
-        node.classList.remove('mouseOut');
-      });
-      node.addEventListener('mouseout', () => {
-        node.classList.add('mouseOut');
-        node.classList.remove('mouseIn');
-      });
-    });
-  }
-
-  function setupScroll() {
-    const menuIcons = document.getElementById('menu-icons');
     Array.from(menuIcons.children).forEach(child => {
+      child.addEventListener('mouseover', () => {
+        child.classList.add('mouseIn');
+        child.classList.remove('mouseOut');
+      });
+      child.addEventListener('mouseout', () => {
+        child.classList.add('mouseOut');
+        child.classList.remove('mouseIn');
+      });
       child.addEventListener('click', (e) => {
         e.preventDefault();
         document.getElementById(child.dataset.scroll).scrollIntoView({ behavior: 'smooth' });
       });
     });
   }
+
+  function setupScroll() {
+    
+  }
   
   document.addEventListener('DOMContentLoaded', function() {
-    setupAnimationListeners();
+    setupMenu();
     setupScroll();
   });
 })();
